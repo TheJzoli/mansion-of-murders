7de7d772b3c9abc242e2a1abf039d551d22354aa
Joel <33924866+TheJzoli@users.noreply.github.com>
2017-11-23 11:24:43 +0200

    added synonym list

--- look.py
+++ look.py
@@ -1,5 +1,7 @@
 import sql
 
+Look = ['look', 'eye', 'glance', 'glimpse', 'peek', 'view', 'gander', 'gaze', 'inspect', 'leer', 'observe', 'watch']
+
 def show_room (room_id):
 	message = "You are in {0}.\n".format (sql.get_room_name(room_id))
 	adjacent_rooms = sql.get_adjacent_rooms(room_id)
