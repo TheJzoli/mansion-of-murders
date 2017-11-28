9f2a4cd0d3cf668435b3f032fadabcebdee7b339
Joel <33924866+TheJzoli@users.noreply.github.com>
2017-11-28 10:16:33 +0200

    move ready

--- move.py
+++ move.py
@@ -6,6 +6,7 @@ short_directions = [] 	# Directions ids
 long_directions = [] 	# Full names of directions
 '''
 import sql
+print("INIT MOVE")
 
 current_room = 1
 
@@ -16,9 +17,17 @@ def move(target):
 		adjacent_rooms = sql.get_adjacent_rooms(current_room)
 		if target_room_id in adjacent_rooms:
 			current_room = target_room_id
-			message = 'Moved to {0}'.format(target)
+			message = 'Moved to the {0}'.format(target)
 		else:
 			message = "You can't move there!"
-
-		
+	elif target in directions:
+		available_directions = sql.get_available_directions(current_room)
+		if target in available_directions:
+			target_room = sql.get_target_room_id(target)
+			current_room = target_room
+			message = 'Moved to the {0}'.format(sql.get_target_room_name)
+		else:
+			message = "You can't move there!"
+	else:
+		message = "That is not somewhere you can go!"
 	return message
