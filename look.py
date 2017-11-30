2ea0689968f6584fb31e90fbb68ae3ac295707a0
Joel <33924866+TheJzoli@users.noreply.github.com>
2017-11-30 09:58:24 +0200

    changed names n stuff

--- look.py
+++ look.py
@@ -12,30 +12,26 @@ import sql
 
 live_npcsid_in_room = sql.live_npcsid_in_room(player.location)
 dead_npcsid_in_room = sql.dead_npcsid_in_room(player.location)
-get_room_id = sql.get_room_id
-id_from_name = sql.id_from_name
+get_room_id = sql.get_room_id(player.location)
+id_from_name = sql.id_from_name(target)
 
 def look(target):
         message = ""
         if(target in rooms):
-                room_id = sql.get_room_id(target)
-                if(room_id == current_room):
-                        query = "SELECT description FROM room WHERE room.room_id ='" + room_id + "';"
+                
+                if(get_room_id == player.location):
+                        query = "SELECT description FROM room WHERE room.room_id ='" + get_room_id + "';"
                         result = sql.query_single(query)
                         message = result
                 else:
                         message = "You can't look there from here."
         elif(target in npcs):
                 
-                npc_id = sql.id_from_name(target)
-                live_npc_id_in_room = sql.live_in_room(npc_id)
-                dead_npc_id_in_room = sql.dead_in_room(npc_id)
-                
-                if(npc_id in live_npc_id_in_room):
-                        query = "SELECT description FROM npc WHERE npc_id ='" + npc_id + "';"
+                if(id_from_name in live_npcsid_in_room):
+                        query = "SELECT description FROM npc WHERE npc_id ='" + id_from_name + "';"
                         result = sql.query_single(query)
                         message = result
-                if(npc_id in dead_npc_id_in_room):
+                if(id_from_name in dead_npcsid_in_room):
                         message = "Here lies the dead body of'" + target + "';" 
                 else:
                         message = "They're not here."
