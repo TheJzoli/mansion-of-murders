30706ea03c9b868797a22deb8f5d3a45a75e9caf
Noora <nooratt@gmail.com>
2017-12-04 11:32:58 +0200

    blame look

--- look.py
+++ look.py
@@ -8,52 +8,74 @@ player					# player instance, holds location
 '''
 import sql
 
-#onko jo olemassa
-## Heh, aika paljon kommentteja, mut nyt tää yhteys enimmäkseen toimii
-## ainakin kuitenkin kun kattoo ketä npc:tä huoneessa on, niin ne näkyy oikein,
-## mutta kun yrittää kattoo vaik 'look at benetton', niin sit tää sanoo, että se ei ole täällä
-## poista toki näitä sitä mukaa ku oot käyny läpi
-## laitoin tollasen kyhäyksen tosta look_aroundista, niin jatka siitä ja tee kivan näkönen
+def all_npcids_in_room(room_id):
+        query = "SELECT npc.npc_id FROM npc, mapped_npc WHERE npc.npc_id = mapped_npc.npc AND mapped_npc.location = '" + str(room_id) + "';"
+        result = sql.column_as_list(sql.run_query(query),0)
+        return result
 
+#No target, just looking around	
+def look_around ():
+	message = "You are in {0}.\n".format(sql.get_room_name(player.location))
+	#siirretty
+	query = "SELECT description FROM room WHERE room.room_id ='" + str(player.location) + "';"
+	result = sql.query_single(query)
+	#toimiiko tässä
+	message += "You can move to:\n"
+	passages = sql.get_adjacent_rooms(player.location)
+	for item in passages:
+		message += "\t{0}\n".format(sql.get_room_name(item))
+	npcs = all_npcids_in_room(player.location)#dead or alive, muotoilu lauseeseen
+	if len(npcs) > 0:
+		message += "These people are here:\n"
+		print(npcs)
+		for item in npcs:
+                        print(item)
+                        message += "\t{0}\n".format(sql.npc_name_from_id(item))
+	else:
+		message += "There is no one else here."
+	return message
+
+def single_npc_description(id_from_name):
+        query = "SELECT description FROM npc WHERE npc_id = " + str(id_from_name) + ";"
+        result = sql.query_single(query)
+        return result
+
+def single_npc_details():
+        query = "SELECT description FROM detail, npc_detail WHERE detail.detail_id = npc_detail.detail AND npc_detail.npc = '" + str(id_from_name) + "';"
+        return column_as_list(run_query(query),0)
 
 def look(target):
-
+## mutta kun yrittää kattoo vaik 'look at benetton', niin sit tää sanoo, että se ei ole täällä
 	live_npcsid_in_room = sql.live_npcsid_in_room(player.location)
 	dead_npcsid_in_room = sql.dead_npcsid_in_room(player.location)
-	#room_id = sql.get_room_id(player.location)
-
 
 	message = ""
 	if(target in rooms):
-			## Noora muutin ja siirsin tän tänne
+                
 			room_id = sql.get_room_id(target)
-			if(room_id == player.location):
-					
-					## Noora tee sileen et tää osio, ja toi look around ois sama,
-					## Tyyliin et tää vaan kutsuu look_around()
 			
-					query = "SELECT description FROM room WHERE room.room_id ='" + str(room_id) + "';"
-					result = sql.query_single(query)
-					message = result
-					
-					## Tää on testi vaa, kun ei oo nitä descriptioneja vielä
+			if(room_id == player.location):
+					message = look_around()
 					if message == None:
 						message = "{0} is rather nice.".format(target)
 			else:
 					message = "You can't look there from here."
 	elif(target in npcs):
 			
-			## NOORA laitoin vaan tän tähän
-			id_from_name = sql.id_from_name(target)
+			target_id = sql.npc_id_from_name(target)
+			print(sql.room_name_from_id(player.location))
+			for npc in live_npcsid_in_room:
+                                print(sql.npc_name_from_id(npc))
 			
-			
-			## Tänne pitää kans laittaa ne detailit näkyviin
-			
-			if(id_from_name in live_npcsid_in_room):
-					query = "SELECT description FROM npc WHERE npc_id = " + str(id_from_name) + ";"
-					result = sql.query_single(query)
-					message = result
-			if(id_from_name in dead_npcsid_in_room):
+			if(target_id in live_npcsid_in_room):
+                                
+					message = single_npc_description(target_id)
+					details = single_npc_detail()#lista
+					message += "\n They have the following details: \n"
+					for detail in details:
+                                                message += "\t{0}\n".format(detail)
+					
+			if(target_id in dead_npcsid_in_room):
 					message = "Here lies the dead body of'" + str(target) + "';" 
 			else:
 					message = "They're not here."
@@ -64,28 +86,6 @@ def look(target):
 	else:
 		message = "Why would you look at that?"
 	
-	return message
-
-	
-## NOORA voit salee käyttää tätä tossa ylemmässä jos target on pelaajan location	
-#No target, just looking around	
-def look_around ():
-	message = "You are in {0}.\n".format(sql.get_room_name(player.location))
-	
-	message += "You can move to:\n"
-	passages = sql.get_adjacent_rooms(player.location)
-	for item in passages:
-		message += "\t{0}\n".format(sql.get_room_name(item))
-	
-	
-	## elävät vaan nyt, pitää muuttaa
-	npcs = sql.live_npcsid_in_room(player.location)
-	if len(npcs) > 0:
-		message += "These NPCS are here:\n"
-		for id in npcs:
-			message += "\t{0}\n".format(sql.npc_name_from_id(id))
-	else:
-		message += "There are no one here."
 	return message
 
                 
