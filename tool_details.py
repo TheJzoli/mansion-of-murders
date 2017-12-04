ba5e3ca1e2fbf65893e6a57ff5847fce4222d893
Shophorn <kauppasarvi@gmail.com>
2017-12-04 22:16:03 +0200

    npc detail tool and more details

--- tool_details.py
+++ tool_details.py
@@ -1,8 +1,87 @@
 # Detail Tool
 # ©Leo Tamminen
 
-import random;
+import random
+import math
 
+import mysql.connector
+database = mysql.connector.connect(
+					host = 'localhost',
+					user = 'dbuser',
+					passwd = 'dbpass',
+					db = 'mom_game',
+					buffered = True)
+cursor = database.cursor()
+
+
+def get_groups(groups):
+	for i in range (len(groups)):
+		cursor.execute("SELECT npc_id FROM npc WHERE map_group = '{0}' AND sub_group = {1};".format(groups[i][0], groups[i][1]))
+		result = cursor.fetchall()
+		
+		list = []
+		for record in result:
+			list.append(record[0])
+		
+		yield list
+
+def shuffle (source):
+	count = len(source)
+	result = list(source)
+	for i in range (count - 1):
+		random_index = random.randint(i, count - 1)
+		result[i], result[random_index] = result[random_index], result[i]
+	return result
+	
+def shuffled_details (source):
+	details = shuffle(source)
+	for item in details:
+		yield item
+		
+group_ids_a = ['A1', 'A2', 'A3', 'A4', 'A5']
+groups_a = []
+for item in get_groups(group_ids_a):
+	groups_a.append (item)
+	
+group_ids_b = ['B1', 'B2', 'B3', 'B4', 'B5']
+groups_b = []	
+for item in get_groups(group_ids_b):
+	groups_b.append (item)
+
+	
+cursor.execute("SELECT COUNT(*) FROM detail")	
+number_details = cursor.fetchall()[0][0]
+
+half = math.ceil(number_details / 2)
+details = []
+for i in range (1, number_details + 1):
+	details.append(i)
+
+details_a = details [:half]
+details_b = details [half:]
+
+details_per_npc = 5
+npc_details = []
+
+for group in groups_a:
+	details = shuffled_details(details_a)
+	for npc in group:
+		for _ in range (details_per_npc):
+			npc_details.append((npc, details.__next__()))
+
+for group in groups_b:
+	details = shuffled_details(details_b)
+	for npc in group:
+		for _ in range (details_per_npc):
+			npc_details.append ((npc, details.__next__()))
+			
+
+printout = ""
+for item in sorted(npc_details):
+	printout += "INSERT INTO npc_detail VALUES ({0:3}, {1:3});\n".format(item[0], item[1])
+print (printout)
+	
+'''
 people = 20
 details = 20
 details_per_person = 5
@@ -29,3 +108,5 @@ for m in range (people):
 	for n in range (details_per_person):
 		detail = detail_lists [m] [n]
 		print ("INSERT INTO npc_detail VALUES ({0}, {1});".format (person, detail))
+		
+'''
--- tool_npc.py
+++ tool_npc.py
@@ -1,6 +1,8 @@
 # NPC Tool
 # © Leo Tamminen, leo.tamminen@metropolia.fi
 
+import math
+
 def trim (text):
 	while len(text) > 0 and text [0] in char_removables:
 		text = text[1:]
@@ -16,7 +18,40 @@ def input_list (message):
 		processed_words.append (trim(word))
 		
 	return processed_words
+	
+def format_name(name):
+	first_name = name[0][0].upper() + name[0][1:]
+	last_name = name[1][0].upper() + name[1][1:]
+	return "{0} {1}".format(first_name, last_name)
+	
+def divide_to_parts(count, parts):
+	count = math.ceil(count)
+	for i in range (count):
+		yield int(i/(count/parts)) + 1
 
+def indices (list):
+	count = len(list)
+	for i in range (count):
+		yield i + 1
+
+def npc_index_input (text):
+	target = input(text).lower().split()
+	
+	index = None
+	
+	if len(target) == 2 and target in names:
+		index = names.index(target)
+
+	elif len(target) == 1:
+		try:
+			target = int (target[0])
+			if target in indices(names):
+				index = target - 1
+		except:
+			pass
+		
+	return index
+		
 print (
 		"Welcome to NPC Tool.\n\n"
 		
@@ -24,19 +59,18 @@ print (
 		" 'd'	edit description\n"
 		" 's'	show all\n"
 		" 'r'	remove person\n"
+		" 'p'	print SQL-queries\n"
 		" 'l'	load NPCs from database\n"
 		" 'e'	exit\n"
 		)
-		
-		
-cmd_add_person = ['n']
+
+cmd_add_person = ['n', 'name']
 cmd_add_description = ['d']
 cmd_show = ['s']
 cmd_remove = ['r']
-cmd_load = ['l']
-cmd_exit = ['e']
-
-commands = ['n','d','s','r','l','e']
+cmd_print = ['p']
+cmd_load = ['l', 'load']
+cmd_exit = ['e', 'exit']
 
 cmd_yes = ['y', 'Y']
 char_removables = [' ']
@@ -44,7 +78,10 @@ char_removables = [' ']
 names = []
 longest_length = 0
 descriptions = []
-max_chars_in_description = 30
+max_chars_in_description = 40
+
+map_groups = ['A', 'B']
+sub_groups = [1,2,3,4,5]
 
 generic_description = "Generic description."
 
@@ -55,8 +92,11 @@ while not command in cmd_exit:
 	while command is None:
 		command = input(">>> ").lower()
 	
+	## ADD NEW PERSON ---------------------------------------------------------
 	if command in cmd_add_person:
-		names_input = input("Enter full names separated with comma:\n\n").lower().split(sep = ',')
+		#names_input = input("Enter full names separated with comma:\n\n").lower().split(sep = ',')
+
+		names_input = input_list("Enter full names separated with comma:\n\n")
 		
 		added_names = []
 		for name in names_input:
@@ -65,33 +105,36 @@ while not command in cmd_exit:
 				names.append(name)
 				descriptions.append(generic_description)
 				added_names.append (name)
+				length = len(name [0]) + len(name[1])
+				if length > longest_length:
+					longest_length = length
 		
 		print ("Added: {0}".format(added_names))
 	
+	## EDIT DESCRIPTION -------------------------------------------------------
 	elif command in cmd_add_description:
 		
-		target = input("\nWhose description do you want to edit? Enter full name:\n\n").lower().split()
-		if len(target) == 2 and target in names:
-			index = names.index(target)
-			print ("\nCurrent description: {0}".format(descriptions[index]))
+		index = npc_index_input("\nWhose description do you want to edit? Enter full name or index:\n\n")
+		
+		if not index is None:
+			print ("\n{0}\nCurrent description: {1}".format(format_name(names[index]), descriptions[index]))
 			confirm = trim(input("Edit? (y)"))
 			
 			if confirm in cmd_yes:
 				description = trim(input("\nEnter new description:\n\n"))
-			
+		
 				if len(description) > 0:
 					descriptions[index] = description
 					print ("Done!")
 		
+	## PRINT IN CONSOLE -------------------------------------------------------
 	elif command in cmd_show:
 	
 		count = len(names)
 		printout = ""
 		for i in range(count):
 			index = i + 1
-			first_name = names[i][0][0].upper() + names[i][0][1:]
-			last_name = names[i][1][0].upper() + names[i][1][1:]
-			name = "{0} {1}".format(first_name, last_name)
+			name = format_name(names [i])
 			
 			if len(descriptions[i]) > max_chars_in_description:
 				description = descriptions[i][:max_chars_in_description - 3] + "..."
@@ -99,13 +142,57 @@ while not command in cmd_exit:
 				description = descriptions[i]
 			
 			printout += "{0:3} {1:{2}} {3}\n".format(index, name, longest_length + 1, description)
+			
+			# Add extra line after every tenth
+			if (i + 1) % 10 == 0:
+				printout += "\n"
 		
 		print(printout)
+	
+	## PRINT SQL --------------------------------------------------------------
+	elif command in cmd_print:
+		confirm = trim(input("Print SQL-queries? (y) "))
 		
+		if confirm in cmd_yes:
+			printout = ""
+			count = len(names)
+			
+			# Two map_groups, two sets of sub_groups
+			map_group_indices = divide_to_parts (count, 2)
+			sub_group_indices = [divide_to_parts(count/2, 5), divide_to_parts(count/2, 5)]
+						
+			for i in range(count):
+				index = i + 1
+				first_name = names[i][0]
+				last_name = names[i][1]
+				description = descriptions[i]
+				
+				map_group_index = map_group_indices.__next__() - 1
+				map_group = map_groups[map_group_index]
+				sub_group = sub_group_indices[map_group_index].__next__()
+				
+				printout += "INSERT INTO npc VALUES ({0}, '{1}', '{2}', '{3}', '{4}', {5});\n".format(index, first_name, last_name, description, map_group, sub_group)
+	
+			print(printout)	
+	
+	## REMOVE NPC -------------------------------------------------------------
 	elif command in cmd_remove:
-		print ("Remove")
+		index = npc_index_input("Which NPC you want to delete? Enter full name or index:\n\n")
 		
-		
+		if not index is None:
+			confirm = trim(input("Do you want to delete '{0}'? (y) ".format(format_name(names[index]))))
+			
+			if confirm in cmd_yes:
+				del names[index]
+				del descriptions[index]
+				print ("Removed!")
+				
+				longest_length = 0
+				for name in names:
+					length = len(name[0]) + len(name[1])
+					if length > longest_length:
+						longest_length = length
+			
 	## LOAD FROM DATABASE -----------------------------------------------------
 	elif command in cmd_load:
 		confirm = trim(input("Load NPCs from database? (y) "))
